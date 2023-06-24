9ef0afb6a6fb14a8127ca7332ef7811b10de90d4
meisyarahhma <meisyarahma6@students.unnes.ac.id>
2023-06-24 12:11:51 +0700

    update footer di landing

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,14 +1,16 @@
-<!-- Footer-->
-    <footer class="py-5 bg-dark" id="footer">
-            <section class="d-flex justify-content-center text-white">
-                <!-- Left -->
-                <div class="me-5 d-none d-lg-block">
-                <span>Get connected with us on social networks: </span>
-                </div>
-                <!-- Left -->
 
-                <!-- Right -->
-                <div>
+    <!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
                     <a href="https://facebook.com/meisyarahhma" class="me-4 text-reset">
                         <i class="bi bi-facebook"></i>
                     </a>
@@ -22,7 +24,93 @@
                         <i class="bi bi-github"></i>
                     </a>
                 </div>
-                <!-- Right -->
-            </section> </br>
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Meisya Rahmasari FSWD2 2023</p></div>
-    </footer>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Buket Mekar
+          </h6>
+          <p>
+            Buket Mekar adalah toko online yang menjual berbagai macam produk buket untuk kado wisuda dan ulang tahun.
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Buket Bunga</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Buket Bunga + Boneka</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Buket Snack</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Buket Barang</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Pricing</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Settings</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Orders</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Help</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3"></i> Semarang, Jawa Tengah</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            buketmekar@example.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2023 Copyright:
+    <a class="text-reset fw-bold" href="">Meisya Rahmasari</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
